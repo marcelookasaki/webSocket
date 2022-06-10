c9b9f0fa5502a21fcfd264eb10223bb94d465396
marcelookasaki <marcelo.okasaki@gmail.com>
2022-06-10 18:54:32 -0300

    FAQ

--- servidor.js
+++ servidor.js
@@ -10,9 +10,33 @@ wss.on('connection', (ws) => {
   console.log('conexao foi estabelecida')
   ws.on('message', data => {
     console.log(`Ecoa que o cliente  enviou: ${data}`)
+    // Verifica a resposta de acordo com a pergunta
+    /* 
+    1 - Pergunta 1
+    */
+    switch (data.toString()[0]) {
+      case '1':
+        resposta = 'Resposta 1'
+        break
+      case '2':
+        resposta = 'Resposta  2'
+        break
+      case '3':
+        resposta = 'Resposta  3'
+        break
+      case '4':
+        resposta = 'Resposta  4'
+        break
+      case '5':
+        resposta = 'Resposta  5'
+        break
+      default:
+        console.log('Pergunta inválida')
+    }
     // Broadcasting
     for (c in clientes) {
       clientes[c].send('Eco:' + data)
+      clientes[c].send('Resposta:' + resposta)
     }
   })
   ws.on('close', () => {
